91a132e73e07db688cf8f9be60b5f7243a6befcc
Deep Gaurav <deepgauravraj@gmail.com>
2023-05-04 09:38:10 +0530

    add group query

--- src/schema/query.rs
+++ src/schema/query.rs
@@ -1,6 +1,11 @@
 use async_graphql::{Context, Object, SimpleObject, Union};
 
-use crate::{auth::AuthTypes, models::user::User};
+use crate::{
+    auth::AuthTypes,
+    models::{group::Group, user::User},
+};
+
+use super::get_pool_from_context;
 
 pub struct Query;
 
@@ -17,9 +22,31 @@ impl Query {
         match auth_type {
             AuthTypes::UnAuthorized => Err(anyhow::anyhow!("Unauthorized")),
             AuthTypes::AuthorizedNotSignedUp(phone) => {
-                Ok(UserAuth::Unregistered(Unregistered { phone: phone }))
+                Ok(UserAuth::Unregistered(Unregistered { phone }))
             }
             AuthTypes::AuthorizedUser(user) => Ok(UserAuth::Registered(Registered { user })),
+        }
+    }
+
+    pub async fn group<'a>(&self, context: &Context<'a>, id: String) -> anyhow::Result<Group> {
+        let auth_type = context
+            .data::<AuthTypes>()
+            .map_err(|e| anyhow::anyhow!("{e:#?}"))?;
+        match auth_type {
+            AuthTypes::UnAuthorized => Err(anyhow::anyhow!("Unauthorized")),
+            AuthTypes::AuthorizedNotSignedUp(_phone) => Err(anyhow::anyhow!("Unauthorized")),
+            AuthTypes::AuthorizedUser(user) => {
+                let pool = get_pool_from_context(context).await?;
+                let group = Group::get_from_id(&id, pool)
+                    .await
+                    .map_err(|_e| anyhow::anyhow!("Group not found"))?;
+                let users = group.get_users(pool).await?;
+                if users.iter().any(|u| u.id == user.id) {
+                    Ok(group)
+                } else {
+                    Err(anyhow::anyhow!("Unauthorized"))
+                }
+            }
         }
     }
 }
