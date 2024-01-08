2e725ba1700b38ede95fcf0028092c776dfcacc8
Deep Gaurav <deepgauravraj@gmail.com>
2024-01-08 14:19:14 +0530

    better email

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -46,20 +46,43 @@ pub async fn send_email_otp(to_email: &str, otp: &str) -> anyhow::Result<()> {
             EmailContent{
                 mime:"text/html".to_string(),
                 value: r##"
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bill Divide</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+    <title>Your One-Time Passcode for Bill Divide Signup/Login</title>
+</head>
+<body style="margin: 0; padding: 0;">
+    <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+    <div style="margin:50px auto;width:70%;padding:20px 0">
+        <div style="border-bottom:1px solid #eee">
+        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bill Divide</a>
+        </div>
+        <p style="font-size:1.1em">Hi,</p>
+        <p>This is your One-Time Passcode for Bill Divide Signup/Login. Passcode is valid for 5 minutes</p>
+        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{{{PASSCODE}}}</h2>
+        <p style="font-size:0.9em;">Regards,<br />Bill Divide</p>
+        <hr style="border:none;border-top:1px solid #eee" />
     </div>
-    <p style="font-size:1.1em">Hi,</p>
-    <p>This is your One-Time Passcode for Bill Divide Signup/Login. Passcode is valid for 5 minutes</p>
-    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{{{PASSCODE}}}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Bill Divide</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-  </div>
-</div>
+    </div>
+</body>
+</html>
                 "##.replace("{{{PASSCODE}}}", otp)
+            },
+
+            EmailContent{
+                mime:"text/plain".to_string(),
+                value: r##"Hello There,
+
+Your One-Time Passcode for Bill Divide Signup/Login : {{{PASSCODE}}}
+Passcode is valid for 5 minutes
+
+Regards,
+Bill Divide
+"##.replace("{{{PASSCODE}}}", otp)
             }
+
         ],
     };
     let request = REQWEST_CLIENT
@@ -89,7 +112,8 @@ pub async fn send_email_invite(to_email: &str, inviter: &str) -> anyhow::Result<
             name: None,
             email: to_email.to_string(),
         }],
-        subject: "Join [INVITER_NAME] on Bill Divide for Easy Expense Sharing".replace("[INVITER_NAME]", inviter),
+        subject: "Join [INVITER_NAME] on Bill Divide for Easy Expense Sharing"
+            .replace("[INVITER_NAME]", inviter),
         content: vec![
             EmailContent{
                 mime:"text/html".to_string(),
@@ -119,6 +143,18 @@ pub async fn send_email_invite(to_email: &str, inviter: &str) -> anyhow::Result<
 </body>
 </html>        
                 "##.replace("[INVITER_NAME]", inviter)
+            },
+            EmailContent{
+                mime:"text/plain".to_string(),
+                value: r##"Hello There!
+
+You've been invited by [INVITER_NAME] to join Bill Divide, a convenient app to share expenses seamlessly with friends.
+Follow the below link to start dividing bills with ease!
+https://billdivide.app/
+
+Regards,
+Bill Divide
+"##.replace("[INVITER_NAME]", inviter)
             }
         ],
     };
