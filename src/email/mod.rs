735e80d26af84244f752e387d36817cb8f251ea1
Deep Gaurav <deepgauravraj@gmail.com>
2024-01-08 14:09:25 +0530

    change to billdivide

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -31,8 +31,8 @@ pub async fn send_email_otp(to_email: &str, otp: &str) -> anyhow::Result<()> {
     let auth_tok = std::env::var("EMAIL_AUTH_TOK")?;
     let email_payload = EmailPayload {
         from: EmailContact {
-            name: "Split Buddy".to_string().into(),
-            email: "split@deepwith.in".to_string(),
+            name: "Bill Divide".to_string().into(),
+            email: "otp@billdivide.app".to_string(),
         },
         reply_to: vec![],
         cc: vec![],
@@ -41,7 +41,7 @@ pub async fn send_email_otp(to_email: &str, otp: &str) -> anyhow::Result<()> {
             name: None,
             email: to_email.to_string(),
         }],
-        subject: "Your One-Time Passcode for SplitBuddy Signup/Login".to_string(),
+        subject: "Your One-Time Passcode for Bill Divide Signup/Login".to_string(),
         content: vec![
             EmailContent{
                 mime:"text/html".to_string(),
@@ -49,12 +49,12 @@ pub async fn send_email_otp(to_email: &str, otp: &str) -> anyhow::Result<()> {
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
   <div style="margin:50px auto;width:70%;padding:20px 0">
     <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Split Buddy</a>
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bill Divide</a>
     </div>
     <p style="font-size:1.1em">Hi,</p>
-    <p>This is your One-Time Passcode for SplitBuddy Signup/Login. Passcode is valid for 5 minutes</p>
+    <p>This is your One-Time Passcode for Bill Divide Signup/Login. Passcode is valid for 5 minutes</p>
     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{{{PASSCODE}}}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Split Buddy</p>
+    <p style="font-size:0.9em;">Regards,<br />Bill Divide</p>
     <hr style="border:none;border-top:1px solid #eee" />
   </div>
 </div>
@@ -79,8 +79,8 @@ pub async fn send_email_invite(to_email: &str, inviter: &str) -> anyhow::Result<
     let auth_tok = std::env::var("EMAIL_AUTH_TOK")?;
     let email_payload = EmailPayload {
         from: EmailContact {
-            name: "Split Buddy".to_string().into(),
-            email: "split@deepwith.in".to_string(),
+            name: "Bill Divide".to_string().into(),
+            email: "invite@billdivide.app".to_string(),
         },
         reply_to: vec![],
         cc: vec![],
@@ -89,7 +89,7 @@ pub async fn send_email_invite(to_email: &str, inviter: &str) -> anyhow::Result<
             name: None,
             email: to_email.to_string(),
         }],
-        subject: "Join [INVITER_NAME] on Split Buddy for Easy Expense Sharing".replace("[INVITER_NAME]", inviter),
+        subject: "Join [INVITER_NAME] on Bill Divide for Easy Expense Sharing".replace("[INVITER_NAME]", inviter),
         content: vec![
             EmailContent{
                 mime:"text/html".to_string(),
@@ -99,20 +99,20 @@ pub async fn send_email_invite(to_email: &str, inviter: &str) -> anyhow::Result<
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-    <title>Join Split Buddy - Expense Sharing Made Easy</title>
+    <title>Join Bill Divide - Expense Sharing Made Easy</title>
 </head>
 <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; margin: 0; padding: 0;">
     <table role="presentation" width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
         <td style="padding: 20px;">
-        <h2 style="font-size: 24px;">Join Split Buddy - Expense Sharing Made Easy!</h2>
+        <h2 style="font-size: 24px;">Join Bill Divide - Expense Sharing Made Easy!</h2>
         <p>Hello there!</p>
-        <p>You've been invited by <strong>[INVITER_NAME]</strong> to join Split Buddy, a convenient app to share expenses seamlessly with friends. <strong>[INVITER_NAME]</strong> wants to share an expense with you!</p>
-        <p style="margin-bottom: 20px;">To start splitting bills hassle-free, simply <a href="https://split.deepwith.in/" style="display: inline-block; padding: 10px 20px; text-decoration: none; background-color: #007bff; color: #fff; border-radius: 3px;">Join Now</a></p>
+        <p>You've been invited by <strong>[INVITER_NAME]</strong> to join Bill Divide, a convenient app to share expenses seamlessly with friends. <strong>[INVITER_NAME]</strong> wants to share an expense with you!</p>
+        <p style="margin-bottom: 20px;">To start splitting bills hassle-free, simply <a href="https://billdivide.app/" style="display: inline-block; padding: 10px 20px; text-decoration: none; background-color: #007bff; color: #fff; border-radius: 3px;">Join Now</a></p>
         <p>If the button above doesn't work, you can copy and paste the following link into your browser:</p>
-        <p style="margin-bottom: 20px;">https://split.deepwith.in/</p>
+        <p style="margin-bottom: 20px;">https://billdivide.app/</p>
         <p>We're excited to have you on board!</p>
-        <p>Best regards,<br>Your Split Buddy Team</p>
+        <p>Best regards,<br>Your Bill Divide Team</p>
         </td>
     </tr>
     </table>
